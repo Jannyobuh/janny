f94e2439941939476447ba5c01f08450e20dfffd
Jannyobuh <93395007+Jannyobuh@users.noreply.github.com>
2021-10-30 15:36:51 +0100

    first upload

---
+++ app.js
@@ -0,0 +1,133 @@
+// const menu = document.querySelector('#mobile-menu');
+// const menuLinks = document.querySelector('.navbar__menu');
+
+// menu.addEventListener('click', function() {
+//     menu.classList.toggle('is-active');
+//     menuLinks.classList.toggle('active');
+// });
+
+
+
+
+// //var studName = "Janny";
+// //studName = "You are welcome"
+// //alert(studName);
+// // var score = 'seven';
+// // var score2 = 'nine';
+// // var total = score + score2;
+// // alert(total);
+// // function add(num1,num2) {
+// //     var divide = num1 / num2;
+    
+// //     return add;
+// // }
+// // function sub(a, b) {
+// //     var sub = a - b;
+   
+// //     return subtraction;
+// // }
+
+// // document.write(add(12, 2))
+// document.write(sub(12, 2))
+// function myName(firstName,  middleName,  lastName) {
+    
+//     // return (firstName + ' ' + middleName + ' ' + lastName);
+//     return `${firstName} ${middleName} ${lastName}`;
+// }
+// document.write(myName("Obuh",  "Chidimma",   "Janny"))
+
+// IF STATEMENT 27/10/21
+// if (5>10){
+//     document.write("Yes")
+// }else if (5!==5){
+//     document.write("Not Equal");
+// }else if('5'===5){
+//     document.write("wrong");
+// }else if(5>3){
+//     document.write("Correct");
+// }else{
+//     document.write("the end");
+// }
+// var mark = 1;
+// function grade(score){
+//     if (score < 100 && score >= 80){
+//         return "A";
+//     }else if(score < 79 && score >= 70){
+//         return "B";
+//     }   else if(score < 69 && score >= 50){
+//         return "C";
+//     }else if(score < 49 && score >= 45){
+//             return "D";
+//     }else if(score < 44 && score >= 40){
+//         return "E";
+//     }else if(score < 39 && score >= 0){
+//         return "F";
+//     }else{
+//     return "Error, the score is invalid";
+//     }
+// }
+// document.write(grade(mark))
+
+// switch ("A") {
+//     case "A":
+//         num1 = 6;
+//         num2 = 4;
+//         result = num1 * num2
+//         console.log(result);
+//         break;
+   
+//     default:
+//         break;
+// }
+
+// var a = 12
+// var b = 13
+// var c;
+
+// switch ("add") {
+//     case "multiplies":
+//         c = a * b;
+//         console.log(c);
+//         break;
+
+//     case "divide":
+//         c = a / b;
+//         console.log(c);
+//         break;
+
+//     case "add":
+//         c = a + b;
+//         console.log(c);
+//         break;
+
+//     default:
+//         break;
+// }
+
+// LOOP 
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// LOOP CHALLENGE
+// var n = 2;
+// var m = 12;
+// function timeTable (n,m){
+//     for (i = 1; i <= m; i++){
+//         console.log(n + ' x ' + i + ' = ' + n * i);
+//     }
+
+
+// }
+
+// for (var i = 1; 1 < 20; i+=2){
+//     if(i%2!=0)
+//     document.write(i)
+// }
+// for (var i = 20; i > 0; i-=2) {
+//     if (i % 2 === 0 ){
+//     console.log(i);
+//     }
+// }
+
+
